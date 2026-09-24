d0e715404357ea1965e8ce99307aca22a5c5abf7
Chen Sakkol <chensakkol1124@gmail.com>
2026-09-24 23:39:17 +0700

    fix typo in Main Class (#7)

--- Main.java
+++ Main.java
@@ -1,1 +1,3 @@
-public class Main { public static void main(String[] args) { System.out.println("System v1.1 - Auth Module Loaded"); } }
+
+public class Main { public static void main(String[] args) { System.out.println("System v1.1"); }}
+
